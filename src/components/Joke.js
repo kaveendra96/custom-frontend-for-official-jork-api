58ae6b268fcc88c8707530c85a8f9f1fa5bca2ff
kaveendra96 <kaveendra96@gmail.com>
2020-11-02 22:45:30 +0530

    create Jokes component

---
+++ src/components/Joke.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Joke = () =>{
+    return(
+        <div className="container">
+            <div className="alert alert-success"></div>
+        </div>
+    )
+}
+export default Joke
---
+++ src/components/Jokes.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Joke from './Joke'
+
+class Jokes extends React.Component{
+
+    state={jokes:[]}
+
+    async componentDidMount(){
+       await fetch('https://official-joke-api.appspot.com/random_ten')
+            .then(res => res.json())
+                .then(jokes => this.setState({jokes}))
+
+    }
+
+
+    render(){
+        const { jokes } = this.state
+        return(
+            <div>
+                {
+                    jokes.map(joke => {
+                        return(
+                            <Joke key={joke.id} joke={joke}></Joke>
+                     ) })
+                }
+            </div>
+        )
+    }
+}
+export default Jokes
